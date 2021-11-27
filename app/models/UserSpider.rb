6454e81c0026df793f4689f0e36204195b251e59
Lily <lolpo12345@outlook.com>
2021-11-27 22:27:39 +0300

    Create base file for User

--- app/models/UserSpider.rb
+++ app/models/UserSpider.rb
@@ -25,28 +25,15 @@ class UserSpider < Kimurai::Base
       response = browser.current_response
     end
 
-    if response.css('div.col-sm-12.content-main > div.character-profile').empty?
+    if response.css('div.col-sm-12.content-main > div.user-profile').empty?
       return nil
     end
 
-    character[:name] = response.css('h1.display-4').text
-    character[:owner] = {}
-    character[:owner][:name] = response.css('span.display-user > a')[0].text
-    character[:owner][:profile] = response.css('span.display-user a')[0]['href']
-    character[:creator] = {}
-    character[:creator][:name] = response.css('span.display-user > a')[2].text
-    character[:creator][:profile] = response.css('span.display-user a')[2]['href']
-    character[:description] = ""
-    response.css('div.profile-content-content.user-content p').each { |p| character[:description] += "#{p.text}\n" }
-    character[:fav_count] = response.css('div.fields-field > dd.col-sm-8')[2].text.strip
-    character[:created_n_ago] = response.css('abbr.tooltipster.datetime')[0].text
-    from_time = Time.now - character[:created_n_ago][0..1].to_i.days - character[:created_n_ago][9..10].to_i.hours
-    character[:created_at] = from_time.strftime("%d %b %Y")
-    character[:recent_images] = []
-    response.css('li.gallery-item').each { |li| character[:recent_images].push li.css('a.img-thumbnail')[0]["href"] }
+    user = {}
+    
 
 
-    print character[:description].split("\n")
-    return character
+
+    return user
   end
 end
