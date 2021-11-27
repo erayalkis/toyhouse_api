696fccb9487597f49cf23e6d9bd2e14bbd3354d1
Lily <lolpo12345@outlook.com>
2021-11-27 19:19:19 +0300

    Improve creator and desc fetching

--- app/models/CharacterSpider.rb
+++ app/models/CharacterSpider.rb
@@ -30,36 +30,28 @@ class CharacterSpider < Kimurai::Base
     end
 
     character[:name] = response.css('h1.display-4').text
-    character[:owner] = {}
-    character[:owner][:name] = response.css('span.display-user > a')[0].text
-    character[:owner][:profile] = response.css('span.display-user a')[0]['href']
     character[:creator] = {}
-    character[:creator][:name] = response.css('span.display-user > a')[2].text
-    character[:creator][:profile] = response.css('span.display-user a')[2]['href']
+    character[:creator][:name] = response.css('dd.field-value.col-sm-8')[1].text
+    character[:creator][:profile] = response.css('dd.field-value.col-sm-8 a')[0]['href']
+    character[:creator][:profile] = nil if response.css('dd.field-value.col-sm-8')[1].children.length == 1
+    character[:owner] = {}
+    character[:owner][:name] = response.css('div.profile-name-info > h2 > .display-user > a').text
+    character[:owner][:profile] = response.css('div.profile-name-info > h2 > .display-user > a')[0]['href']
     character[:description] = ""
-    response.css('div.profile-content-content.user-content').children.each do |ele|
-      unless ele.children.empty?
-        ele.children.each { |ele| character[:description] += "#{ele.text}\n" if ele.name != 'br' }
-      else
-        character[:description] += "#{ele.text}\n" if ele.children.length > 1
-      end    
+    if response.css('div.profile-content-content.user-content').children.length == 1
+      character[:description] = response.css('div.profile-content-content.user-content').text 
+    else
+      response.css('div.profile-content-content.user-content').children.each do |ele|
+        unless ele.children.empty?
+          ele.children.each { |ele| character[:description] += "#{ele.text}\n" if ele.name != 'br' }
+        else
+          character[:description] += "#{ele.text}\n" if ele.children.length > 1
+        end    
+      end
     end
     character[:fav_count] = response.css('div.fields-field > dd.col-sm-8')[2].text.strip
     character[:created_n_ago] = response.css('abbr.tooltipster.datetime')[0].text
-    if character[:created_n_ago][0..1].start_with?('day')
-      time1 = character[:created_n_ago][0..1].to_i.days
-    else
-      character[:created_n_ago][0..1].start_with?('minute')
-      time1 = character[:created_n_ago][0..1].to_i.minutes
-    end
-    if character[:created_n_ago][9..10].start_with?('hour')
-      time2 = character[:created_n_ago][9..10].to_i.hours
-    else
-     time2 = character[:created_n_ago][9..10].to_i.seconds
-    end
-    from_time = Time.now - time1 - time2
-    character[:created_n_ago][0..1] 
-    character[:created_at] = from_time.strftime("%dth of %b %Y")
+    character[:created_at] = response.css('dl.fields > div.row.fields-field > dd.field-value.col-sm-8 > abbr')[0]['title']
     character[:tags] = []
     response.css('div.profile-tags-content a').each { |a| character[:tags] << a.text }
     character[:recent_images] = []
