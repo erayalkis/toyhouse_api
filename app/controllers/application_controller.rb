6d82f060a5d62cd3f03dab77c7a928d0c316079f
Lily <lolpo12345@outlook.com>
2021-12-03 18:06:05 +0300

    Fix typo in application#app_status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
 
   def app_status 
-    respond json: { status: 200, msg: 'Application Online'  }, status: :ok
+    render json: { status: 200, msg: 'Application Online'  }, status: :ok
   end
 
 end
