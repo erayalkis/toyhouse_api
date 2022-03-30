15c03fc93dd84f1c2cf58071d4ae2a1ead063dc9
Lily <lolpo12345@outlook.com>
2022-03-30 22:53:22 +0300

    Move cookie to credentials

--- app/models/CharacterGallerySpider.rb
+++ app/models/CharacterGallerySpider.rb
@@ -4,7 +4,7 @@ class CharacterGallerySpider < Kimurai::Base
   @engine = :mechanize
   @config = {
     user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36",
-    cookies: [{name: "laravel_session", value: "eyJpdiI6InFPK1dKTS9ReVJuRnBDR1Zzcjdsa2c9PSIsInZhbHVlIjoiTE9sZmdxUkZZbzdEbVdRbUxRZkpwY3V2L3JXd2kwN0h6TkdqR1hoSWl6dDRXREplaGFRNWs4RjZhOFEyZCt0MzVTSVM1clpQRTJzZ2lGSU9WMXZzNEJ2d3U1VDdHSHVMdzFWeUJxTTgzV2R3dTNydlFJaTMwMjZQR0VsbldkdG4iLCJtYWMiOiJiOGEyODU2ZDA0MGRiMmFkMGMxOGNlZmUxZTM4NGI5YTliZmNjZmQzNDAyNTNmOTBkMGRjYmQ3OGZkODhmNTc2In0%3D", domain: "toyhou.se"}]
+    cookies: [{name: "laravel_session", value: Rails.application.credentials.account_cookie, domain: "toyhou.se"}]
   }
   @auths = nil
 
