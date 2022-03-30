3f0126aa38295cf6ca2c4ab09ef3f144257bbf3c
Lily <lolpo12345@outlook.com>
2022-03-30 22:56:13 +0300

    Move cookies to credentials and merge conflicts

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -55,16 +55,15 @@ class RequestController < ApplicationController
       return render json: { msg: 'Please pass in a Toyhouse profile ID!' }, status: 404
     end
 
-    begin 
+    begin
       if params[:id] && params[:gallery_only] == "true"
-        response = CharacterGallerySpider.instance("https://toyhou.se/#{params[:id]}/gallery", @auths)
+        response = CharacterGallerySpider.instance("https://toyhou.se/#{params[:id]}/gallery")
       elsif params[:id] && params[:details_only] == "true"
         response = CharacterDetailsSpider.instance("https://toyhou.se/#{params[:id]}")
       else
         response = CharacterSpider.instance("https://toyhou.se/#{params[:id]}")
       end
-    rescue => err
-      puts err 
+    rescue
       render json: { 
         msg: "Invalid Toyhouse link or private profile.", 
         msg_desc: "The profile you're trying to fetch has custom HTML or it is a locked profile.", 
@@ -100,16 +99,16 @@ class RequestController < ApplicationController
 
   end
 
-  private 
-
-  def clear_cache(path)
-    FileUtils.rm_rf(Dir[]) 
-  end
+  private
 
   def get_authorizations
     # Use a set for holding auths so that lookup time is O(1) !!!!!!!!!!!!!!!!!!!!!!!
     # Pass the auths to the instance method of CharacterGallerySpider (and maybe others) and check if the profile name on the 
       # page matches any in the auths set to authorize users
     @auths = AuthorizationsSpider.instance("https://toyhou.se/~account/authorizers")
+  end
+  
+  def clear_cache(path)
+    FileUtils.rm_rf(Dir[]) 
   end
 end
--- app/models/CharacterGallerySpider.rb
+++ app/models/CharacterGallerySpider.rb
@@ -6,35 +6,26 @@ class CharacterGallerySpider < Kimurai::Base
     user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36",
     cookies: [{name: "laravel_session", value: Rails.application.credentials.account_cookie, domain: "toyhou.se"}]
   }
-  @auths = nil
 
-  def self.instance(url, auths)
+  def self.instance(url)
     @start_urls = [url]
-    @auths = auths
 
-    gallery = self.parse!(:parse, url: @start_urls[0], data: {auths: @auths})
+
+    gallery = self.parse!(:parse, url: @start_urls[0])
     return gallery
   end
 
   def parse(response, url:, data: {})
     character = {}
-    character[:owner] = {}
-    character[:owner][:name] = response.css('span.display-user > a > span.display-user-username').text
 
+   
+    unless response.css('input.btn-success').empty?
+      browser.click_button response.css('input.btn-success')[0]['value']
+      response = browser.current_response
+    end
     if response.css('ul.magnific-gallery').empty?
       return { msg: 'Character has no images or character profile is locked!', status: 422 }
     end
-
-    unless response.css('i.fa.fa-unlock-alt').empty?
-      if data[:auths].include?(character[:owner][:name])
-        puts "User is authorized"
-      else
-        return { msg: 'Character is locked and/or you\'re unauthorized to see their profile!', status: 422 }
-      end
-    end
-
-
-    character[:owner][:link] = response.css('span.display-user > a')[0]['href']
     character[:name] = response.css('li.character-name').text.strip
     character[:gallery] = []
     response.css('div.thumb-image > a').each { |a| character[:gallery] << a['href'] }
