011316eaaf95e08358ed786f6b213cd562ffa814
Lily <lolpo12345@outlook.com>
2022-04-27 20:03:11 +0300

    Fix route action names for requests controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/character', to: 'request#scrape_character_profile'
-  get '/user', to: 'request#scrape_user_profile'
+  get '/character', to: 'requests#scrape_character_profile'
+  get '/user', to: 'requests#scrape_user_profile'
   get '/app_status', to: 'application#app_status'
 end
