c1a30be45b6bb8729a811168401b6dbb506d6823
Lily <lolpo12345@outlook.com>
2022-04-30 03:13:39 +0300

    Nest current test code under #scrape_character_profile

--- spec/controllers/requests_controller_spec.rb
+++ spec/controllers/requests_controller_spec.rb
@@ -2,31 +2,35 @@ class RequestControllerSpec
 
   describe RequestsController do
 
-    before(:all) do     
-      @character = {
-        url: "https://toyhou.se/15895178.test-character-",
-        id: "15895178.test-character-"
-      }
+    describe "#scrape_character_profile" do
+      before(:all) do     
+        @character = {
+          url: "https://toyhou.se/15895178.test-character-",
+          id: "15895178.test-character-"
+        }
+      end
+
+      it "should return a 404 Not Found error when an id isn't passed in" do
+        get :scrape_character_profile
+        assert_response :not_found
+      end
+
+          
+      it "should fetch data successfully" do
+        get :scrape_character_profile, :params => { id: @character[:id] }
+        # Just updating the response instance variable so I don't make repeated requests
+        # Don't wanna accidentally DOS the server yknow
+        assert_response :success
+      end
+
+      it "should fetch the correct data" do
+        get :scrape_character_profile, :params => { id: @character[:id] }
+        data = JSON.parse(response.body)
+        expect(data["name"]).to eq("test character!!!")
+      end
     end
 
-    it "should not fetch data when an id isn't passed in" do
-      get :scrape_character_profile
-      assert_response :not_found
-    end
 
-        
-    it "should fetch data successfully" do
-      get :scrape_character_profile, :params => { id: @character[:id] }
-      # Just updating the response instance variable so I don't make repeated requests
-      # Don't wanna accidentally DOS the server yknow
-      assert_response :success
-    end
-
-    it "should fetch the correct data" do
-      get :scrape_character_profile, :params => { id: @character[:id] }
-      data = JSON.parse(response.body)
-      expect(data["name"]).to eq("test character!!!")
-    end
       
 
   end
