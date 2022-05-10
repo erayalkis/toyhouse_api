3281c13f98002f5ed9f3ab25a04767f471fa6746
Lily <lolpo12345@outlook.com>
2022-05-10 03:06:58 +0300

    Update CharGallerySpider to not skip over empty credits

--- app/services/spiders/CharacterGallerySpider.rb
+++ app/services/spiders/CharacterGallerySpider.rb
@@ -26,7 +26,6 @@ class Spiders::CharacterGallerySpider < Kimurai::Base
     character = {}
     character[:owner] = {}
     character[:owner][:name] = response.css('span.display-user > a > span.display-user-username').text
-    artists = response.css('div.artist-credit')
 
     if response.css('ul.magnific-gallery').empty?
       return { msg: 'Character has no images or character profile is locked!', status: 422 }
@@ -43,8 +42,11 @@ class Spiders::CharacterGallerySpider < Kimurai::Base
     character[:owner][:link] = response.css('span.display-user > a')[0]['href']
     character[:name] = response.css('li.character-name').text.strip
     character[:gallery] = []
-    response.css('div.thumb-image > a').each_with_index do |a, i| 
-      name = artists[i].text.strip
+
+    response.css('li.gallery-item').each_with_index do |item, idx| 
+      name = item.css('div.artist-credit').text.strip
+      a = item.css('div.thumb-image > a')[0]
+      
       character[:gallery] << { link: a['href'], artist: { name: name, profile: get_profile_name(name)} }
     end
     return character
