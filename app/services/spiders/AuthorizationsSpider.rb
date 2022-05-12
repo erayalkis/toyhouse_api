bdd487a329c55f90fb28ca431b16f7cbc9eef35f
Lily <lolpo12345@outlook.com>
2022-05-12 20:22:24 +0300

    Update code for fetching cookies

--- app/services/spiders/AuthorizationsSpider.rb
+++ app/services/spiders/AuthorizationsSpider.rb
@@ -29,14 +29,16 @@ class Spiders::AuthorizationsSpider < Kimurai::Base
     auths = Set.new
 
     unless response.css("form.form-horizontal").empty?
-      browser.fill_in "login_username", match: :first, with: "toyhouse_downloader"
-      browser.fill_in "login_password", match: :first, with: Rails.application.credentials.account_password
-      browser.click_button "Sign in", match: :first
-      response = browser.current_response
-      new_cookie = browser.driver.get_cookies[0]
+      data = [["login_username", "toyhouse_downloader"],
+        ["login_password", Rails.credentials.account_cookie]]
 
+      uri = URI('https://toyhou.se/~account/login')
+      response = Net::HTTP.post_form(uri, data)
+
+      new_cookie = response['Set-Cookie']
+ 
       access_yaml = YAML.load_file(Rails.root.join('config', 'access_cookie.yml'))
-      access_yaml["account_cookie"] = new_cookie.value
+      access_yaml["account_cookie"] = new_cookie.split(";")[5].split(",")[1].split("=")[1].strip
 
       File.open(Rails.root.join('config', 'access_cookie.yml'), 'w+') { |f| YAML.dump(access_yaml, f) }
 
@@ -46,10 +48,6 @@ class Spiders::AuthorizationsSpider < Kimurai::Base
     if response.css('div.row.align-items-end').empty?
       return Set.new
     end
-
-    # unless response.css('i.fa.fa-unlock-alt').empty?
-    #   return { msg: 'Character is locked and/or you\'re unauthorized to see their profile!', status: 422 }
-    # end
 
     usernames = response.css("a.btn.btn-sm.user-name-badge")
     usernames.each do |username|
