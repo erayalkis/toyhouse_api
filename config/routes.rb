af63148a52b89263c93ceb780baf62f0d2ad9f7c
erayalkis <erayalkis@hotmail.com>
2022-05-27 23:59:13 +0300

    Update route handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/character', to: 'requests#scrape_character_profile'
-  get '/user', to: 'requests#scrape_user_profile'
+  namespace :character do
+    get '/:id', to: 'characters#profile'
+    get '/:id/gallery', to: 'characters#gallery'
+    get '/:id/details', to: 'characters#details'
+    get '/:id/gallery', to: 'characters#favorites'
+  end
+
+  namespace :user do
+    get '/:id', to: 'users#profile'
+  end
+
   get '/app_status', to: 'application#app_status'
-  get '/cache_gallery', to: 'requests#cache_gallery'
-  get '/download_gallery', to: 'requests#download_gallery'
 end
