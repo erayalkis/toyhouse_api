a5297a46ac85c0e68bfefa19770d9808d26c0436
erayalkis <erayalkis@hotmail.com>
2022-05-30 00:08:05 +0300

    Finish tests for characters#gallery

--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -45,9 +45,9 @@ class CharactersControllerSpec
 
         expect(data["name"]).to eq("test character!!!")
         expect(data["creator"]["name"]).to eq("toyhouse_downloader")
-        expect(data["creator"]["profile"]).to eq("https://toyhou.se/toyhouse_downloader")
+        expect(data["creator"]["link"]).to eq("https://toyhou.se/toyhouse_downloader")
         expect(data["owner"]["name"]).to eq("toyhouse_downloader")
-        expect(data["owner"]["profile"]).to eq("https://toyhou.se/toyhouse_downloader")
+        expect(data["owner"]["link"]).to eq("https://toyhou.se/toyhouse_downloader")
         expect(data["description"]).to eq("here!!!!\n")
         # Value might change, but will always be more than 0
         expect(data["fav_count"].to_i).to be > 0 
@@ -70,9 +70,9 @@ class CharactersControllerSpec
 
         expect(data["name"]).to eq(" 🌊 yui !! 🌊")
         expect(data["creator"]["name"]).to eq("SPARKNIGHT")
-        expect(data["creator"]["profile"]).to eq("https://toyhou.se/SPARKNIGHT")
+        expect(data["creator"]["link"]).to eq("https://toyhou.se/SPARKNIGHT")
         expect(data["owner"]["name"]).to eq("kyumi")
-        expect(data["owner"]["profile"]).to eq("https://toyhou.se/kyumi")
+        expect(data["owner"]["link"]).to eq("https://toyhou.se/kyumi")
         # Description might change, but a length higher than 0 means we're fetching the description
         expect(data["description"].length).to be > 0
         # Value might change, but will always be more than 0
@@ -97,45 +97,40 @@ class CharactersControllerSpec
         expect(data["name"]).to eq("test character!!!")
       end
 
-      # it "should fetch all data corectly" do
-      #   get :profile, :params => { id: @character[:id] }
-      #   data = JSON.parse(response.body)
+      it "should fetch all data corectly" do
+        get :gallery, :params => { id: @character[:id] }
+        data = JSON.parse(response.body)
 
-      #   expect(data["name"]).to eq("test character!!!")
-      #   expect(data["creator"]["name"]).to eq("toyhouse_downloader")
-      #   expect(data["creator"]["profile"]).to eq("https://toyhou.se/toyhouse_downloader")
-      #   expect(data["owner"]["name"]).to eq("toyhouse_downloader")
-      #   expect(data["owner"]["profile"]).to eq("https://toyhou.se/toyhouse_downloader")
-      #   expect(data["description"]).to eq("here!!!!\n")
-      #   # Value might change, but will always be more than 0
-      #   expect(data["fav_count"].to_i).to be > 0 
-      # end
+        expect(data["owner"]["name"]).to eq("toyhouse_downloader")
+        expect(data["owner"]["link"]).to eq("https://toyhou.se/toyhouse_downloader")
+        expect(data["name"]).to eq("test character!!!")
+        # Length must be equal to or higher than gallery length at the time of writing tests 
+        # It can also go lower if a bunch of images end up getting deleted, but this is the most reliable way to test this
+        expect(data["gallery"].length).to be >= 1 
+      end
 
-      # it "should fetch data from authorized characters" do
-      #   get :profile, :params => { id: @authorized_character[:id] }
-      #   assert_response :success
-      # end
+      it "should fetch data from authorized characters" do
+        get :gallery, :params => { id: @authorized_character[:id] }
+        assert_response :success
+      end
 
-      # it "should fetch the correct data from authorized characters" do
-      #   get :profile, :params => { id: @authorized_character[:id] }
-      #   data = JSON.parse(response.body)
-      #   expect(data["name"]).to eq(" 🌊 yui !! 🌊")
-      # end
+      it "should fetch the correct data from authorized characters" do
+        get :gallery, :params => { id: @authorized_character[:id] }
+        data = JSON.parse(response.body)
+        expect(data["name"]).to eq("🌊 yui !! 🌊")
+      end
 
-      # it "should fetch all data correctly from authorized characters" do
-      #   get :profile, :params => { id: @authorized_character[:id] }
-      #   data = JSON.parse(response.body)
+      it "should fetch all data correctly from authorized characters" do
+        get :gallery, :params => { id: @authorized_character[:id] }
+        data = JSON.parse(response.body)
 
-      #   expect(data["name"]).to eq(" 🌊 yui !! 🌊")
-      #   expect(data["creator"]["name"]).to eq("SPARKNIGHT")
-      #   expect(data["creator"]["profile"]).to eq("https://toyhou.se/SPARKNIGHT")
-      #   expect(data["owner"]["name"]).to eq("kyumi")
-      #   expect(data["owner"]["profile"]).to eq("https://toyhou.se/kyumi")
-      #   # Description might change, but a length higher than 0 means we're fetching the description
-      #   expect(data["description"].length).to be > 0
-      #   # Value might change, but will always be more than 0
-      #   expect(data["fav_count"].to_i).to be > 0
-      # end
+        expect(data["owner"]["name"]).to eq("kyumi")
+        expect(data["owner"]["link"]).to eq("https://toyhou.se/kyumi")
+        expect(data["name"]).to eq("🌊 yui !! 🌊")
+        # Length must be equal to or higher than gallery length at the time of writing tests
+        # It can also go lower if a bunch of images end up getting deleted, but this is the most reliable way to test this
+        expect(data["gallery"].length).to be >= 23
+      end
     end
 
     
