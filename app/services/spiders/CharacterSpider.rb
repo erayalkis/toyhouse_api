51a64bbc92ed45919c5cc83cf8c9b1aaad06b3bf
erayalkis <erayalkis@hotmail.com>
2022-05-29 19:07:50 +0300

    Add cookies for CharacterSpider

--- app/services/spiders/CharacterSpider.rb
+++ app/services/spiders/CharacterSpider.rb
@@ -6,9 +6,16 @@ class Spiders::CharacterSpider < Kimurai::Base
     user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36",
   }
 
-  def self.instance(url)
+  def self.instance(url, auths)
     @start_urls = [url]
-
+    @auths = auths
+    @config[:cookies] = [
+      {
+        name: "laravel_session", 
+        value: YAML.load_file(Rails.root.join('config', 'access_cookie.yml'))['account_cookie'],
+        domain: "toyhou.se"
+      }
+    ]
 
     character = self.parse!(:parse, url: @start_urls[0])
     return character
@@ -17,13 +24,16 @@ class Spiders::CharacterSpider < Kimurai::Base
   def parse(response, url:, data: {})
     character = {}
 
-    unless response.css('input.btn-success').empty?
-      browser.click_button response.css('input.btn-success')[0]['value']
-      response = browser.current_response
-    end
-
     if response.css('div.col-sm-12.content-main > div.character-profile').empty?
       return nil
+    end
+
+    unless response.css('i.fa.fa-unlock-alt').empty?
+      if data[:auths].include?(character[:owner][:name])
+        puts "User is authorized"
+      else
+        return { msg: 'Character is locked and/or you\'re unauthorized to see their profile!', status: 422 }
+      end
     end
 
     character[:name] = response.css('h1.display-4').text.strip
