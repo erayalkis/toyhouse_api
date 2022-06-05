f4409bc46c0671998826ba568e3e9fa0988ff08b
erayalkis <erayalkis@hotmail.com>
2022-06-05 14:24:45 +0300

    Make Service Object calls DRY

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -2,19 +2,19 @@ class CharactersController < ApplicationController
   before_action :ensure_id_exists
 
   def profile
-    fetch_and_send_response(params[:id], "default")
+    fetch_and_send_response("default")
   end
 
   def details
-    fetch_and_send_response(params[:id], "details_only")
+    fetch_and_send_response("details_only")
   end
 
   def gallery
-    fetch_and_send_response(params[:id], "gallery_only")
+    fetch_and_send_response("gallery_only")
   end
 
   def favorites
-    fetch_and_send_response(params[:id], "favorites")
+    fetch_and_send_response("favorites")
   end
 
   private
@@ -25,9 +25,14 @@ class CharactersController < ApplicationController
     end
   end
   
-  def fetch_and_send_response(id, scraper_type)
-    data = fetch_character_data(id, scraper_type)
+  def fetch_and_send_response(scraper_type)
+    data = fetch_character_data(scraper_type)
     respond_with_data(data)
+  end
+
+  def fetch_character_data(scraper_type)
+    character_data = SpiderManager::Character.call(params[:id], scraper_type)
+    character_data
   end
 
   def respond_with_data(character_data)
@@ -41,8 +46,4 @@ class CharactersController < ApplicationController
     end
   end
 
-  def fetch_character_data(id, scraper_type)
-    character_data = SpiderManager::Character.call(params[:id], scraper_type)
-    character_data
-  end
 end
