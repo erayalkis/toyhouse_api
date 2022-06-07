ce59059bbbe19ba458c24b4edbd65068b86c0e91
erayalkis <erayalkis@hotmail.com>
2022-06-07 14:52:23 +0300

    Finish code for CharacterComments

--- app/services/spiders/CharacterCommentsSpider.rb
+++ app/services/spiders/CharacterCommentsSpider.rb
@@ -2,18 +2,24 @@ class Spiders::CharacterCommentsSpider < Spiders::ToyhouseSpider
   @name = 'character_comments_spider'
 
   def parse(response, url:, data: {})
-    comments = []
+    character = {}
+    character[:name] = response.css("h1.comments-title a").text.strip
+    character[:comments] = []
 
-    response.css("forum-post-post").each do |comment|
+    response.css("div.forum-post-post").each do |comment|
       curr = {}
       curr[:user] = {}
-      curr[:user][:image] = comment.css("forum-post-avatar img")[0]['href']
-      curr[:user][:username] = comment.css("forum-post-user-badge a").text
-      curr[:body] = comment.css("user-content").text
 
-      comments << curr
+      # Disabled accounts won't have any data and will throw an error when being accessed
+      image = comment.css("div.hidden-xs-down.forum-post-avatar > img")
+      curr[:user][:image] = image.length > 0 ? image[0]['src'] : nil
+      
+      curr[:user][:username] = comment.css("span.forum-post-user-badge a").text.strip
+      curr[:body] = comment.css("div.user-content").text.strip
+
+      character[:comments] << curr
     end
 
-    comments
+    character
   end
 end
