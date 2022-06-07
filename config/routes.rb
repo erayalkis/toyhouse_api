ed17fbce1ccd11b94db974c47a89cea179841bef
erayalkis <erayalkis@hotmail.com>
2022-06-07 14:15:08 +0300

    Set up routes for characters#comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope :character, constraints: { id: /[^\/]+/ } do
-    get '/:id', to: 'characters#profile'
-    get '/:id/gallery', to: 'characters#gallery'
-    get '/:id/details', to: 'characters#details'
+    get '/:id',           to: 'characters#profile'
+    get '/:id/gallery',   to:   'characters#gallery'
+    get '/:id/details',   to:   'characters#details'
     get '/:id/favorites', to: 'characters#favorites'
+    get '/:id/comments',  to:  'characters#comments'
   end
 
   scope :user, constraints: { id: /[^\/]+/ } do
-    get '/:id', to: 'users#profile'
-    get '/:id/subscribers', to: 'users#subscribers'
+    get '/:id',               to: 'users#profile'
+    get '/:id/subscribers',   to: 'users#subscribers'
     get '/:id/subscriptions', to: 'users#subscriptions'
   end
 
