f0a6b764543529898b358ec4e4b772448967179e
erayalkis <erayalkis@hotmail.com>
2022-06-09 04:27:48 +0300

    Move config cookie initialization inside instance method

--- app/services/spiders/ToyhouseSpider.rb
+++ app/services/spiders/ToyhouseSpider.rb
@@ -1,23 +1,25 @@
 class Spiders::ToyhouseSpider < Kimurai::Base
   @engine = :mechanize
-  @config = {
-    user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36",
-    cookies: [
+
+  def self.instance(url, auths=nil)
+    @start_urls = [url]
+    @config = {
+      user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36",
+      cookies: []
+    }
+
+    unless File.exists?(Rails.root.join('config', 'access_cookie.yml'))
+      yaml = {'account_cookie' => nil}
+      File.open(Rails.root.join('config', 'access_cookie.yml'), 'w+') { |f| YAML.dump(yaml, f) }
+    end
+
+    @config[:cookies] = [
       {
         name: "laravel_session", 
         value: YAML.load_file(Rails.root.join('config', 'access_cookie.yml'))['account_cookie'],
         domain: "toyhou.se"
       }
     ]
-  }
-
-  def self.instance(url, auths=nil)
-    @start_urls = [url]
-
-    unless File.exists?(Rails.root.join('config', 'access_cookie.yml'))
-      yaml = {'account_cookie' => nil}
-      File.open(Rails.root.join('config', 'access_cookie.yml'), 'w+') { |f| YAML.dump(yaml, f) }
-    end
 
     data = self.parse!(:parse, url: @start_urls[0], data: { auths: @auths })
     return data
