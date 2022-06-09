cbadd474834410d46275028d68f06be940d1815d
erayalkis <erayalkis@hotmail.com>
2022-06-09 15:42:16 +0300

    Enable custom values for raffle options

--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -10,18 +10,18 @@ class RafflesController < ApplicationController
     favorites[:favorites].each do |favorite| 
       username = favorite[:username]
       tickets[username] = {}
-      tickets[username][:ticket_count] = 1
+      tickets[username][:ticket_count] = params[:fav_ticket_count].to_i || 1
       tickets[username][:image] = favorite[:image]
     end
 
     comments[:comments].each do |comment|
       username = comment[:user][:username]
-      tickets[username][:ticket_count] += 1 if tickets.has_key?(username)
+      tickets[username][:ticket_count] += params[:comment_ticket_count].to_i || 1 if tickets.has_key?(username)
     end
     
     subscribers[:subscribers].each do |subscriber|
       username = subscriber[:username]
-      tickets[username][:ticket_count] += 1 if tickets.has_key?(username) 
+      tickets[username][:ticket_count] += params[:subscribe_ticket_count].to_i || 1 if tickets.has_key?(username) 
     end
 
     render json: tickets
