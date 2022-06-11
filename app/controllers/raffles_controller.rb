95adc03866168e6e3e64de99d28194f968288391
erayalkis <erayalkis@hotmail.com>
2022-06-11 22:48:32 +0300

    Refactor code to make it DRY

--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -1,30 +1,41 @@
 class RafflesController < ApplicationController
 
   def calculate_tickets
-    comments = SpiderManager::Character.call(params[:id], "comments")
-    favorites = SpiderManager::Character.call(params[:id], "favorites")
-    owner_id = comments[:owner][:name]
-    subscribers = SpiderManager::User.call(owner_id, "subscribers")
-
     tickets = {}
+
+    favorites = SpiderManager::Character.call(params[:id], "favorites")
     favorites[:favorites].each do |favorite| 
       username = favorite[:username]
       tickets[username] = {}
-      tickets[username][:ticket_count] = params[:fav_ticket_count].to_i || 1
+      tickets[username][:ticket_count] = set_ticket_count_if_exists("fav")
       tickets[username][:image] = favorite[:image]
     end
 
+    owner_id = comments[:owner][:name]
+
+
+    comments = SpiderManager::Character.call(params[:id], "comments")
     comments[:comments].each do |comment|
       username = comment[:user][:username]
-      tickets[username][:ticket_count] += params[:comment_ticket_count].to_i || 1 if tickets.has_key?(username)
+      tickets[username][:ticket_count] += set_ticket_count_if_exists("comment") if tickets.has_key?(username)
     end
     
+    subscribers = SpiderManager::User.call(owner_id, "subscribers")
     subscribers[:subscribers].each do |subscriber|
       username = subscriber[:username]
-      tickets[username][:ticket_count] += params[:subscribe_ticket_count].to_i || 1 if tickets.has_key?(username) 
+      tickets[username][:ticket_count] += set_ticket_count_if_exists("subscribe") if tickets.has_key?(username) 
     end
 
     render json: tickets
+  end
+
+
+  private
+
+  def set_ticket_count_if_exists(option_type)
+    ticket_count_param = "#{option_type}_ticket_count"
+    return 1 if params[ticket_count_param].nil?
+    params[ticket_count_param]
   end
 
 end
