22bc0b936b0be7cd43025132e91a4e8a88ee9c01
Eray Alkış <lolpo12345@outlook.com>
2022-08-30 01:37:22 +0300

    Fix auth

--- app/services/spiders/AuthorizationsSpider.rb
+++ app/services/spiders/AuthorizationsSpider.rb
@@ -6,28 +6,36 @@ class Spiders::AuthorizationsSpider < Spiders::ToyhouseSpider
 
     unless response.css("form.form-horizontal").empty?
       xsrf_token_input = response.xpath('//input[@name="_token"]/@value')[1].value
-      cookie_jar = HTTParty::CookieHash.new
 
       data = {
+        "_token": xsrf_token_input,
         "username": Rails.application.credentials.toyhouse_account[:username],
-        "password": Rails.application.credentials.toyhouse_account[:password],
-        "_token": xsrf_token_input
+        "password": Rails.application.credentials.toyhouse_account[:password]
       }
 
+      cookie_jar = HTTParty::CookieHash.new
+
+      puts browser.driver.get_cookies
+      session_cookie = browser.driver.get_cookies[0]
       xsrf_token_cookie = browser.driver.get_cookies[1]
+      cookie_jar.add_cookies(session_cookie.to_s)
       cookie_jar.add_cookies(xsrf_token_cookie.to_s)
 
       headers = {
-        'XSRF-TOKEN' => cookie_jar.to_cookie_string,
-        'Content-Type' => 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': '100',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-ch-ua': '"Opera GX";v="89", "Chromium";v="103", "_Not:A-Brand";v="24"',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 OPR/89.0.4447.104',
+        'Cookie': cookie_jar.to_cookie_string
       }
 
-      response = HTTParty.post('https://toyhou.se/~account/login', { body: URI.encode_www_form(data), headers: headers  } )
+      response = HTTParty.post('https://toyhou.se/~account/login', { body: URI.encode_www_form(data), headers: headers })
 
       new_cookie = response.headers['Set-Cookie']
-
       access_yaml = YAML.load_file(Rails.root.join('config', 'access_cookie.yml'))
-      access_yaml["account_cookie"] = new_cookie.split(";")[0].split("=")[1]
+      access_yaml["account_cookie"] = new_cookie.split(", ")[2..4].join(", ").split("=")[1].split(";")[0]
+      access_yaml["xsrf_cookie"] = xsrf_token_cookie
 
       File.open(Rails.root.join('config', 'access_cookie.yml'), 'w+') { |f| YAML.dump(access_yaml, f) }
 
