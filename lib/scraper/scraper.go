b5027c0e3967b3e4ef66d9a6fe696c29d69f5a39
Eray Alkış <lolpo12345@outlook.com>
2022-11-04 00:42:19 +0300

    Set up struct instead of map

--- lib/scraper/scraper.go
+++ lib/scraper/scraper.go
@@ -9,15 +9,16 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-// type Character struct {
-// 	name string
-// }
+type Character struct {
+	Name string `json:"name" binding:"required"`
+	Images []string `json:"images" binding:"required"`
+}
 
 func ScrapeUser() {}
 
-func ScrapeCharacter(user_id string) map[string]string {
+func ScrapeCharacter(user_id string) Character {
 	fmt.Println("Scraping user", user_id)
-	full_url := fmt.Sprint("https://toyhou.se/", user_id)
+	full_url := fmt.Sprint("https://toyhou.se/", user_id, "/gallery");
 	jar, _ := cookiejar.New(nil);
 
 	client := http.Client{
@@ -36,10 +37,16 @@ func ScrapeCharacter(user_id string) map[string]string {
 		log.Fatal(err);
 	}
 
-	name := doc.Find("h1.display-4").Text();
+	name := doc.Find("h1.image-gallery-title a").Text();
+	var images []string;
+ 	doc.Find(".magnific-item").Each(func(i int, ele *goquery.Selection ) {
+		link, _ := ele.Find("img").Attr("href");
+		images = append(images, link);
+	})
 
-	character := map[string]string {
-		"name": name,
+	character := Character {
+		Name: name,
+		Images: images,
 	}
 
 	fmt.Println(character);
