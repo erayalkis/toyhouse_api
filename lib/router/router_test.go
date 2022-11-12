ea4c06842a0f8550582e3a5f12f9c9832915df83
Eray Alkış <lolpo12345@outlook.com>
2022-11-13 02:37:53 +0300

    Write basic test

--- lib/router/router_test.go
+++ lib/router/router_test.go
@@ -1,1 +1,21 @@
 package router_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"toyhouse_api/v2/lib/router"
+
+	"github.com/magiconair/properties/assert"
+)
+
+func TestRouterAlive(t *testing.T) {	
+	router := router.SetRoutes()
+
+	recorder := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	router.ServeHTTP(recorder, req)
+
+	assert.Equal(t, 200, recorder.Code)
+	assert.Equal(t, "pong", recorder.Body.String())
+}
