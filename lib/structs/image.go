c85c06d136f9415602a409c518c32c8814a89102
Eray Alkış <lolpo12345@outlook.com>
2022-11-13 19:54:17 +0300

    Add metadata to Image struct

--- lib/structs/image.go
+++ lib/structs/image.go
@@ -3,4 +3,5 @@ package structs
 type Image struct {
 	Link string `json:"link" binding:"required"`
 	Artists []Profile `json:"artists" binding:"required"`
+	Metadata ImageMetadata `json:"image_metadata" binding:"required"`
 }
