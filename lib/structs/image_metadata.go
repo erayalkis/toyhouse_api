85bf749ca6a308c3b925880dc4e6e502ad8986da
Eray Alkış <lolpo12345@outlook.com>
2022-11-16 23:28:41 +0300

    Fix faulty json name

--- lib/structs/image_metadata.go
+++ lib/structs/image_metadata.go
@@ -3,5 +3,5 @@ package structs
 type ImageMetadata struct {
 	TaggedCharacters []Profile `json:"tagged_characters" binding:"required"`
 	Description string `json:"description" binding:"required"`
-	Date string `json:"string" binding:"required"`
+	Date string `json:"date" binding:"required"`
 }
