e8cbebca0bce2cdec41c1cc4a0322b9c270176da
Eray Alkış <lolpo12345@outlook.com>
2022-11-16 23:28:49 +0300

    Fix faulty json name in Profile struct

--- lib/structs/profile.go
+++ lib/structs/profile.go
@@ -2,5 +2,5 @@ package structs
 
 type Profile struct {
 	Name string `json:"name" binding:"required"`
-	Link string `json:"Link" binding:"required"`
+	Link string `json:"profile" binding:"required"`
 }
