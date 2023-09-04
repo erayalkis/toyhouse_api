00671f845f2e19e37756f184f852e9c0ae9c89ce
Eray Alkış <lolpo12345@outlook.com>
2023-09-04 17:43:06 +0300

    Update compiler for go

--- build-to-bin.sh
+++ build-to-bin.sh
@@ -7,5 +7,7 @@ mv ./gui/src-tauri/bin/main ./gui/src-tauri/bin/main-aarch64-apple-darwin
 cp ./main ./gui/src-tauri/bin/main
 mv ./gui/src-tauri/bin/main ./gui/src-tauri/bin/main-x86_64-apple-darwin
 cp ./main ./gui/src-tauri/bin/main
+mv ./gui/src-tauri/bin/main ./gui/src-tauri/bin/main-x86_64-pc-windows-msvc.exe
+cp ./main ./gui/src-tauri/bin/main
 mv ./gui/src-tauri/bin/main ./gui/src-tauri/bin/main-x86_64-unknown-linux-gnu
 cp ./main ./gui/src-tauri/bin/main
