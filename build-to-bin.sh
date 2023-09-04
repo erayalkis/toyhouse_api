8a1324d2fa269e259b54ee5458600ae1551d42f5
Eray Alkış <lolpo12345@outlook.com>
2023-09-04 17:26:46 +0300

    Update build

--- build-to-bin.sh
+++ build-to-bin.sh
@@ -5,5 +5,7 @@ mkdir ./gui/src-tauri/bin
 cp ./main ./gui/src-tauri/bin/main
 mv ./gui/src-tauri/bin/main ./gui/src-tauri/bin/main-aarch64-apple-darwin
 cp ./main ./gui/src-tauri/bin/main
+mv ./gui/src-tauri/bin/main ./gui/src-tauri/bin/main-x86_64-apple-darwin
+cp ./main ./gui/src-tauri/bin/main
 mv ./gui/src-tauri/bin/main ./gui/src-tauri/bin/main-x86_64-unknown-linux-gnu
 cp ./main ./gui/src-tauri/bin/main
