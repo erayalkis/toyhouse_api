a927a795f02be0705e29eae1f754cef59e27a6f9
Eray Alkış <lolpo12345@outlook.com>
2023-09-07 23:02:34 +0300

    Emit event messages from backend for download tracking

--- gui/src-tauri/src/main.rs
+++ gui/src-tauri/src/main.rs
@@ -1,9 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{fs, path::PathBuf};
-
 use futures::StreamExt;
+use serde::Serialize;
+use std::{fs, path::PathBuf, sync::OnceLock};
+use tauri::{Manager, Window};
+
+#[derive(Serialize, Clone)]
+struct Payload {
+    id: String,
+}
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -21,9 +27,12 @@ async fn download_character(
 ) -> PathBuf {
     let mut download_path = tauri::api::path::download_dir().unwrap();
     let folder_name = format!("{}-gallery", id);
-    download_path.push(folder_name);
+    download_path.push(&folder_name);
 
     fs::create_dir_all(&download_path).unwrap();
+    let window = WINDOW.get().unwrap();
+    let payload = Payload { id: folder_name };
+    window.emit("gallery-begin", payload).unwrap();
 
     download_charcter_gallery(&id, links).await;
     write_character_credits(&id, credits);
@@ -38,11 +47,12 @@ async fn download_character(
 
 async fn download_charcter_gallery(id: &String, links: Vec<String>) {
     let iter = links.into_iter();
+    let window = WINDOW.get().unwrap();
 
     for (idx, mut link) in iter.enumerate() {
         let mut download_path = tauri::api::path::download_dir().unwrap();
         let folder_name = format!("{}-gallery", id);
-        download_path.push(folder_name);
+        download_path.push(&folder_name);
 
         tokio::spawn(async move {
             let qmark_index = link.chars().position(|c| c == '?');
@@ -59,13 +69,28 @@ async fn download_charcter_gallery(id: &String, links: Vec<String>) {
             let full_path = format!("{}/{}", download_path.display(), filename);
 
             let mut tmp_file = tokio::fs::File::create(full_path).await.unwrap();
-            let mut byte_stream = reqwest::get(link).await.unwrap().bytes_stream();
+            let mut byte_stream;
+            match reqwest::get(link).await {
+                Ok(resp) => {
+                    byte_stream = resp.bytes_stream();
+                }
+                Err(_) => {
+                    let payload = Payload {
+                        id: folder_name.clone(),
+                    };
+                    window.emit("gallery-download-error", payload).unwrap();
+                    return;
+                }
+            };
 
             while let Some(item) = byte_stream.next().await {
                 tokio::io::copy(&mut item.unwrap().as_ref(), &mut tmp_file)
                     .await
                     .unwrap();
             }
+
+            let payload = Payload { id: folder_name };
+            window.emit("gallery-download-success", payload).unwrap();
         });
     }
 }
@@ -97,8 +122,15 @@ fn write_character_logs(id: &String, logs: String) {
     fs::write(download_path, logs).unwrap();
 }
 
+static WINDOW: OnceLock<Window> = OnceLock::new();
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            WINDOW.set(window).unwrap();
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet, download_character])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
