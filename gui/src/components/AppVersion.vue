113d2a2aac2d30ce06e499327ec518b5320a5eed
Eray Alkış <lolpo12345@outlook.com>
2023-09-29 21:32:55 +0300

    Create AppVersion.vue

---
+++ gui/src/components/AppVersion.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>Toyhouse Extensions {{ appVersion }}</p>
+</template>
+<script setup lang="ts">
+import { Ref, onBeforeMount, ref } from "vue";
+import { getVersion } from "@tauri-apps/api/app";
+
+onBeforeMount(async () => {
+  const version: string = await getVersion();
+
+  appVersion.value = version;
+});
+
+const appVersion: Ref<string> = ref("");
+</script>
